fb9061dd824ea3ec87336b15963b24f229c3c908
Amrani Houssain <amranidev@gmail.com>
2018-12-03 22:29:27 +0000

    Add Laracombee ServiceProvider

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Amranidev\Laracombee\Providers\LaracombeeServiceProvider::class,
 
         /*
          * Application Service Providers...
