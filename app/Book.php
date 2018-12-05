7ca2a9e3d273e60e41a1996835f32156f4ec7e26
Amrani Houssain <amranidev@gmail.com>
2018-12-05 22:33:53 +0000

    Dfine laracombee props in Book

--- app/Book.php
+++ app/Book.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     protected $fillable = ['title', 'body', 'user_id'];
+
+    /**
+     * Recombee item columns.
+     * 
+     * @var array
+     */
+    public static $laracombee = [
+        'title' => 'string', 
+        'body'  => 'string',
+    ];
 }
