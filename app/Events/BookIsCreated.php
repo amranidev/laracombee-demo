6f2efbb5f7d093688bd8985d996d18d44b632848
Amrani Houssain <amranidev@gmail.com>
2018-12-08 15:32:22 +0000

    Create BookIsAdded event

---
+++ app/Events/BookIsCreated.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Events;
+
+use App\Book;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+
+class BookIsCreated
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Book instance.
+     *
+     * @var \App\Book
+     */
+    public $book;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(Book $book)
+    {
+        $this->book = $book;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
